07ab1d1d34fd16bc88a04906a3afc84fa58dcd1a
Victor Josso <admin@shopoffers.fr>
2020-04-20 18:10:31 +0200

    Modification de l'adresse de telechargement du package

--- crack.py
+++ crack.py
@@ -1,6 +1,6 @@
 import urllib2
 
-down_url = "https://github.com/vpnost/crack_files/raw/master/pyovpn-2.0-py2.7.egg"
+down_url = "https://raw.githubusercontent.com/vpnost/crack_files/master/pyovpn-2.0-py2.7.egg"
 filedata = urllib2.urlopen(down_url)
 f = open("/usr/local/openvpn_as/lib/python2.7/site-packages/pyovpn-2.0-py2.7.egg", "wb")
 f.write(filedata.read())
