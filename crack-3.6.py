832a59e6b7fc18b2a66ced1aa35323c02f3180fd
vpnost <64029084+vpnost@users.noreply.github.com>
2022-03-06 00:02:48 +0100

    Update crack-3.6.py

--- crack-3.6.py
+++ crack-3.6.py
@@ -1,7 +1,8 @@
-import urllib2
+import urllib.request
+import shutil
 
 down_url = "https://raw.githubusercontent.com/vpnost/crack_files/master/pyovpn-2.0-py3.6.egg"
-filedata = urllib2.urlopen(down_url)
-f = open("/usr/local/openvpn_as/lib/python/pyovpn-2.0-py3.6.egg", "wb")
-f.write(filedata.read())
-f.close()
+filename = "/usr/local/openvpn_as/lib/python/pyovpn-2.0-py3.6.egg"
+
+with urllib.request.urlopen(down_url) as response, open(file_name, 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
